75f46715b640b029a07f03ffdfb3018904468eb6
hye-won Jang <skyred5846@naver.com>
2022-04-23 21:04:21 +0900

    슬라이드 위에 글씨 쓰기 position변경

--- ZRONE/건축/script/main-script.js
+++ ZRONE/건축/script/main-script.js
@@ -1,3 +1,31 @@
+/* 메인 배경화면 */
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+dots[slideIndex - 1].className += " active";
+    setTimeout(showSlides, 4000); // Change image every 2 seconds
+}
+
+
+
+
+
+/* 여기서 부터 메인 메뉴 */
 $('.tab_menu_btn').on('click',function(){
   $('.tab_menu_btn').removeClass('on');
   $(this).addClass('on')
